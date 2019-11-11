c525f26606362e14eb0b5295b5fbc21a2d4f9938
Khalilurrehman <khalil_rehman1993@ymail.com>
2019-11-11 16:20:20 +0400

    Updates

---
+++ precache-manifest.cbe077c3ef66840ea7927a38e65700c1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "160fddff3ca71887ebf8da3929244379",
+    "url": "/TechStore/static/media/productsBcg.160fddff.jpeg"
+  },
+  {
+    "revision": "f558808080316ecc89a3",
+    "url": "/TechStore/static/css/main.54329e3a.chunk.css"
+  },
+  {
+    "revision": "39dfc9a9733055c13836",
+    "url": "/TechStore/static/js/runtime~main.7b318905.js"
+  },
+  {
+    "revision": "2b362308d9a07ef7d73f4865cbd132ab",
+    "url": "/TechStore/static/media/logo.2b362308.svg"
+  },
+  {
+    "revision": "f76eea7b862944b6467b",
+    "url": "/TechStore/static/js/2.d0b83fb7.chunk.js"
+  },
+  {
+    "revision": "7709c10e8c30753537d94a6b69d7542a",
+    "url": "/TechStore/static/media/aboutBcg.7709c10e.jpeg"
+  },
+  {
+    "revision": "984874d19d1cdef8dcee5fcea55c082d",
+    "url": "/TechStore/static/media/mainBcg.984874d1.jpeg"
+  },
+  {
+    "revision": "f558808080316ecc89a3",
+    "url": "/TechStore/static/js/main.753806e0.chunk.js"
+  },
+  {
+    "revision": "945afb997e0731b038ed58fadacfeb3f",
+    "url": "/TechStore/static/media/contactBcg.945afb99.jpeg"
+  },
+  {
+    "revision": "9675841294261b773267d557f531a8ff",
+    "url": "/TechStore/static/media/singleProductBcg.96758412.jpeg"
+  },
+  {
+    "revision": "46143c2f25bc4667fac18f5924288217",
+    "url": "/TechStore/static/media/storeBcg.46143c2f.jpeg"
+  },
+  {
+    "revision": "0819a81634391f8518fe325378b8f994",
+    "url": "/TechStore/static/media/defaultBcg.0819a816.jpeg"
+  },
+  {
+    "revision": "f76eea7b862944b6467b",
+    "url": "/TechStore/static/css/2.daf3366c.chunk.css"
+  },
+  {
+    "revision": "2e68c4bd5ecc6b46210c12e641128b4f",
+    "url": "/TechStore/index.html"
+  }
+];
